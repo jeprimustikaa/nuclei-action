01d3a236d1d3a24ffcaa38ac3ab400483f9c7700
wrdmchn <wrdmchn@mail.com>
2022-02-19 23:59:08 +0700

    aa

--- push.sh
+++ push.sh
@@ -1,1 +1,1 @@
-git push https://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@github.com/jeprimustikaa/nuclei-action
+git push https://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@github.com/jeprimustikaa/nuclei-action
